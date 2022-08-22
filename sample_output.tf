609b3ace62b4bd064284ac3864868acc7ac794ba
preethi k <kpreethiyadav579@gmail.com>
2022-08-22 21:22:42 +0530

    adding

--- sample_output.tf
+++ sample_output.tf
@@ -6,12 +6,12 @@ output "sample_output"  {
 
 variable "state" {} # Need to declare empty vartable before it calling
 
-output detail_state {
+output "detail_state" {
     value = "Name of the state : ${var.state}"
 }
 
 variable "country" {}
 
-output detail_country {
+output "detail_country" {
     value = "Name of the country : ${var.country}"
 }
