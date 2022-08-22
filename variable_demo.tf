6fe916c451cc4e85eebd7961045a6fdbc34b2621
preethi k <kpreethiyadav579@gmail.com>
2022-08-22 18:19:55 +0530

    adding

--- variable_demo.tf
+++ variable_demo.tf
@@ -1,3 +1,7 @@
 #Terraform Varaibles has three data types
 # 1) String 2) Number 3) Boolean
 
+#String need to be in "" Double Quotes
+#where as the  Number & boolean ---not required 
+
+#Terraform doesn't support SINGLE Quotes
