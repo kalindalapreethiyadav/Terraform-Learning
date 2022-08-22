b69313d16f10fea2c4e260a32627037df2d6e25a
preethi k <kpreethiyadav579@gmail.com>
2022-08-22 21:18:41 +0530

    adding

--- sample_output.tf
+++ sample_output.tf
@@ -7,3 +7,12 @@ output "sample_output"  {
 variable "state" {}
 
 variable "country" {}
+
+
+output detail_state {
+    value = "Name of the state : ${var.state}"
+}
+
+output detail_country {
+    value = "Name of the country : ${var.country}"
+}
