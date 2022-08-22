d2e14c06efd8ad6589013eb66d2d1f9d3bb40d2d
preethi k <kpreethiyadav579@gmail.com>
2022-08-22 22:39:00 +0530

    adding

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "terraform-ec2" {
+resource "aws_instance" "Hello_preethi-ec2" {
   ami                     = "ami-0aa718de62aea6fbe"
   instance_type           = "t3.micro"
 
