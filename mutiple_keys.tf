208813309ede75ff65c77279507c4d82593ef323
preethi k <kpreethiyadav579@gmail.com>
2022-08-22 18:42:12 +0530

    adding

---
+++ mutiple_keys.tf
@@ -0,0 +1,14 @@
+
+
+variable "var_name2" {
+    default = {
+        course = "Devops",
+        duration = 80,
+        status = false
+        topic = "terraform"
+    }
+}
+
+output "displaying_output" {
+    value = "priting ${var.var_name2[0]} and other details are : ${var.var_name2[1]} ${var.var_name2[2] ${var.var_name2[3]}}"
+}
