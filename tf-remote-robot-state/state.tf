82e51cfffebd3241412596bd776db411f7a4374f
preethi k <kpreethiyadav579@gmail.com>
2022-08-24 22:13:16 +0530

    adding

--- tf-remote-robot-state/state.tf
+++ tf-remote-robot-state/state.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {
         bucket  = "robot-bucket-preethi"
         key     = "sample/terraform.tfstate"
-        region    = "us-east-1"
+        region    = "us-east-1b"
 
     }
 }
