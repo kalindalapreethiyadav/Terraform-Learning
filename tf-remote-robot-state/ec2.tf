c86652c8d9f56f8ac09c10991c48ebe701e27978
preethi k <kpreethiyadav579@gmail.com>
2022-08-24 21:37:49 +0530

    adding

--- tf-remote-robot-state/ec2.tf
+++ tf-remote-robot-state/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "preethi-ec2" {
   ami                     = "ami-0aa718de62aea6fbe"
   instance_type           = "t3.micro"
-  vpc_security_group_ids  =  [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids  =  [aws_security_group.allow_ssh]
 
   tags = {
     Name = "my-first-ec2"
@@ -12,4 +12,29 @@ resource "aws_instance" "preethi-ec2" {
 
 output "privateip" {
   value = aws_instance.preethi-ec2.private_ip
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
 }
