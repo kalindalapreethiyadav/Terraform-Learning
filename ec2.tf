9a70a263c4cd15f489a6380ac58de3444e17cc4e
preethi k <kpreethiyadav579@gmail.com>
2022-08-24 07:27:35 +0530

    adding

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,12 @@
-resource "aws_instance" "Hello_preethi-ec2" {
+resource "aws_instance" "preethi-ec2" {
   ami                     = "ami-0aa718de62aea6fbe"
   instance_type           = "t2.micro"
 
   tags = {
-    Name    = "my-first-ec2"
+    Name = "my-first-ec2"
   }
 }
 
 output "privateip" {
-  value = aws_instance.Hello_preethi-ec2.privateip
+  value = aws_instance.preethi-ec2.privateip
 }
