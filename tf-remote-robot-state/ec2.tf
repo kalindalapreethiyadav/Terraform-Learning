97795216e2d638db5d4607eec428bb1f792e95e1
preethi k <kpreethiyadav579@gmail.com>
2022-08-24 21:51:22 +0530

    adding

--- tf-remote-robot-state/ec2.tf
+++ tf-remote-robot-state/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "preethi-ec2" {
   ami                     = "ami-0aa718de62aea6fbe"
   instance_type           = "t3.micro"
-  #vpc_security_group_ids  =  [aws_security_group.allow_ssh]
+  #vpc_security_group_ids  =  [aws_security_group.allow_ssh.id]
 
   tags = {
     Name = "my-first-ec2"
