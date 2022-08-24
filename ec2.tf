73ee12f2899fb6984d9ba3ca736162dfbe1dfa9e
preethi k <kpreethiyadav579@gmail.com>
2022-08-24 07:39:48 +0530

    adding

--- ec2.tf
+++ ec2.tf
@@ -2,8 +2,11 @@ resource "aws_instance" "preethi-ec2" {
   ami                     = "ami-0aa718de62aea6fbe"
   instance_type           = "t2.micro"
 
+
   tags = {
     Name = "my-first-ec2"
+    ENv = "dev"
+    project = "cc"
   }
 }
 
