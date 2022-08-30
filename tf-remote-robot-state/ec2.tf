cf92afaa0a79c999ed5e494504f7e26cc12ac967
preethi k <kpreethiyadav579@gmail.com>
2022-08-30 21:01:58 +0530

    adding

--- tf-remote-robot-state/ec2.tf
+++ tf-remote-robot-state/ec2.tf
@@ -26,6 +26,7 @@ resource "aws_security_group" "allow_ssh" {
     description      = "ssh from VPC"
     from_port        = 22
     to_port          = 22
+    protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
