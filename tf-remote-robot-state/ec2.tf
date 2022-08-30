4e768d2a769fa3b32a5709b449f87b2f25183156
preethi k <kpreethiyadav579@gmail.com>
2022-08-30 20:57:52 +0530

    adding

--- tf-remote-robot-state/ec2.tf
+++ tf-remote-robot-state/ec2.tf
@@ -1,5 +1,23 @@
 
 
+resource "aws_instance" "preethi-ec2" {
+  ami                     = "ami-00ff427d936335825"
+  instance_type           = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = "my-first-ec2"
+    ENV = "dev"
+    project = "robot"
+  }
+}
+
+output "privateip" {
+  value = aws_instance.preethi-ec2.private_ip
+}
+
+
+
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow ssh inbound traffic"
