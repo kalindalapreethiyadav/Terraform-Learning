b8a0f8d649651ebe44ef2033d5e5454ad898d92f
preethi k <kpreethiyadav579@gmail.com>
2022-08-30 20:17:56 +0530

    adding

--- tf-remote-robot-state/ec2.tf
+++ tf-remote-robot-state/ec2.tf
@@ -1,7 +1,7 @@
 
 
 resource "aws_instance" "preethi-ec2" {
-  ami                     = "185303955401"
+  ami                     = "ami-0aa718de62aea6fbe"
   instance_type           = "t3.micro"
 
   tags = {
