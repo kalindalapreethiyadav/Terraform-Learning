f9d5c31aaffc0a634ea4863b91017eada8fb6a67
preethi k <kpreethiyadav579@gmail.com>
2022-09-01 23:50:43 +0530

    adding

--- examples_MODULE/sg/main.tf
+++ examples_MODULE/sg/main.tf
@@ -10,6 +10,14 @@ resource "aws_security_group" "allow_ssh_preethi" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+ingress {
+    description      = "http"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
