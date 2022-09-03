9efb9528540a4d45b2b3f8d598f65d7d22484163
preethi k <kpreethiyadav579@gmail.com>
2022-09-03 11:40:26 +0530

    adding

--- examples_MODULE/ec2/main.tf
+++ examples_MODULE/ec2/main.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "preethi1-ec2" {
     host     = self.public_ip
   }
     inline = [
-    "ansible-pull -i ${self.public_ip} -e ENV=dev -e COMPONENT=frontend -e TAG_NAME=0.0.2 -e ansible_user=centos -e ansible_password=DevOps321 roboshop-push.yml"
+    "ansible-pull -U https://github.com/kalindalapreethiyadav/Ansible.git -e COMPONENT=frontend -e ENV=dev -e ansible_user=centos -e ansible_password=DevOps321 roboshop-push.yml"
     ]
   }
 }
