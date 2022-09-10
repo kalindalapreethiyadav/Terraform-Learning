859224a5081b17993a7870bfc913805327a25004
preethi k <kpreethiyadav579@gmail.com>
2022-09-10 19:33:30 +0530

    adding

---
+++ create-ec2-module/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+    backend "s3" {
+        bucket  = "robot-bucket-preethi"
+        key     = "modules_exam_recap/terraform.tfstate"
+        region    = "us-east-1"
+
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+  
+}
---
+++ create-ec2-module/ec2/main.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "preethi_vm-ec2" {
+  ami                     = "ami-00ff427d936335825"
+  instance_type           = "t3.micro"
+  vpc_security_group_ids = [var.sg]
+
+variable "sg" {}
+ provisioner "remote-exec" {
+      connection {
+        type     = "ssh"
+        user     = "centos"
+        password = "DevOps321"
+        host     = self.public_ip
+      } 
+    inline = [
+        ls -lrt
+     #"ansible-pull -U https://github.com/b49-clouddevops/ansible.git -e COMPONENT=frontend -e ENV=dev -e TAG_NAME=0.0.1 roboshop.yml"
+      ]
+    }
+}
+
+
+
+output "pub-ip" {
+    value = aws_instance.preethi1_vm-ec2.public_ip
+}
