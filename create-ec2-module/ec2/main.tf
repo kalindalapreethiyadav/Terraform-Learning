16c9e6ec15ab8d327ed23048fbe9e3f6ca11fe76
preethi k <kpreethiyadav579@gmail.com>
2022-09-11 20:47:38 +0530

    adding

--- create-ec2-module/ec2/main.tf
+++ create-ec2-module/ec2/main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "preethi_vm_ec2" {
         host     = self.public_ip
       }
     inline = [
-       "ansible-pull -U https://github.com/kalindalapreethiyadav/Ansible.git -e COMPONENT=frontend -e ENV=dev -e TAG_NAME=0.1.1 roboshop.yml"
+        "ansible-pull -U https://github.com/b49-clouddevops/ansible.git -e COMPONENT=frontend -e ENV=dev -e TAG_NAME=0.12.1 roboshop.yml"
       ]
     }
 }
