b6e929f0de7dfe0b518cf48b94fa6fd9304254a3
preethi k <kpreethiyadav579@gmail.com>
2022-09-11 14:37:17 +0530

    adding

--- create-ec2-module/ec2/main.tf
+++ create-ec2-module/ec2/main.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "preethi_vm_ec2" {
   ami                     = "ami-00ff427d936335825"
   instance_type           = "t3.micro"
   vpc_security_group_ids = [var.sg]
-}
 
  provisioner "remote-exec" {
       connection {
@@ -15,7 +14,7 @@ resource "aws_instance" "preethi_vm_ec2" {
      "ansible-pull -U https://github.com/kalindalapreethiyadav/Ansible.git -e COMPONENT=frontend -e ENV=dev -e TAG_NAME=0.1.1 roboshop.yml"
       ]
     }
-
+}
  
 variable "sg" {}
 
