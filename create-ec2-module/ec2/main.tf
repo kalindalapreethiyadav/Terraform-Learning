9c8e469e8b872c9f496e77659617da9edf390afe
preethi k <kpreethiyadav579@gmail.com>
2022-09-11 13:11:38 +0530

    adding

--- create-ec2-module/ec2/main.tf
+++ create-ec2-module/ec2/main.tf
@@ -2,21 +2,8 @@ resource "aws_instance" "preethi_vm-ec2" {
   ami                     = "ami-00ff427d936335825"
   instance_type           = "t3.micro"
   vpc_security_group_ids = [var.sg]
-
- provisioner "remote-exec" {
-      connection {
-        type     = "ssh"
-        user     = "centos"
-        password = "DevOps321"
-        host     = self.public_ip
-      } 
-    inline = [
-      "curl https://gitlab.com/thecloudcareers/opensource/-/blob/master/ansible/install.sh | sudo bash",
-      "ansible-pull -U https://github.com/kalindalapreethiyadav/Ansible.git -e COMPONENT=frontend -e ENV=dev -e TAG_NAME=0.1.1 roboshop.yml"
-      ]
-    }
 }
-
+ 
 variable "sg" {}
 
 output "pub-ip" {
