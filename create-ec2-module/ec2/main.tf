aac8e9116ef0102cb337e1ad40bb80429d4ce171
preethi k <kpreethiyadav579@gmail.com>
2022-09-11 14:14:44 +0530

    adding

--- create-ec2-module/ec2/main.tf
+++ create-ec2-module/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "preethi_vm_ec2" {
-  ami                     = "ami-00ff427d936335825"
+  ami                     = data.aws_ami.base-ami.image_id
   instance_type           = "t3.micro"
   vpc_security_group_ids = [var.sg]
 }
