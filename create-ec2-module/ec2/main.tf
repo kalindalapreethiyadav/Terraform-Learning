f8cb3666fbbe012086063ff4cb858e9ca0680519
preethi k <kpreethiyadav579@gmail.com>
2022-09-12 17:18:24 +0530

    adding

--- create-ec2-module/ec2/main.tf
+++ create-ec2-module/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "preethi_vm_ec2" {
   ami                     = data.aws_ami.LabImage.image_id
   instance_type           = "t3.micro"
   vpc_security_group_ids  = [var.sg]
-
+}
 variable "sg" {}
 
 output "pub-ip" {
