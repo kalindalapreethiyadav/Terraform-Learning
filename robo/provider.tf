e83eb6c1c8147d2ca45f707a1374e3909151ef81
preethi k <kpreethiyadav579@gmail.com>
2022-09-17 22:48:24 +0530

    adding

--- robo/provider.tf
+++ robo/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "b49-rf-remote-state-bucket"
-    key    = "robot-basic-setup/terraform.tfstate"
+    key    = "robot-basic-setup_New/terraform.tfstate"
     region = "us-east-1"
   }
 }
