1ce1298f55000058a22365ab550934ff2fb8e0ab
preethi k <kpreethiyadav579@gmail.com>
2022-09-18 22:29:49 +0530

    adding

--- robo/main.tf
+++ robo/main.tf
@@ -3,3 +3,9 @@ module "frontend" {
     COMPONENT    = "frontend"
     APP_VERSION  = "0.0.9"
 }
+
+module "cart" {
+    source       = "./ec2"
+    COMPONENT    = "cart"
+    APP_VERSION  = "0.0.9"
+}
