86dcf2d98f94f5121ca6b55f639117483e102507
preethi k <kpreethiyadav579@gmail.com>
2022-09-19 00:04:34 +0530

    adding

--- robo/main.tf
+++ robo/main.tf
@@ -1,6 +1,6 @@
 
 module "component" {
-    for_each = [var.ALL_COMPONENTS]
+    for_each = var.ALL_COMPONENTS
     source       = "./ec2"
     COMPONENT = each.key
     APP_VERSION  = each.value.app_version
