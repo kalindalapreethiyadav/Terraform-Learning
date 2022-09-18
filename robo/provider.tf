4749f02fa06c25462e6f51389331d64a204b4265
preethi k <kpreethiyadav579@gmail.com>
2022-09-18 19:19:34 +0530

    adding

--- robo/provider.tf
+++ robo/provider.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-   bucket  = "robot-bucket-preethi"
-        key     = "modules_exam_recap5/terraform.tfstate"
-        region    = "us-east-1"
+   bucket = "terraform-state"
+   key    = "terraform.tfstate"
+   region    = "us-east-1"
+   role_arn = "arn:aws:iam::185303955401:instance-profile/b49_full_access/terraformer"
   }
 }
 
