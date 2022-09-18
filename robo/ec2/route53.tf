261ee9427d6c597fde625a5b6469c2103884a53e
preethi k <kpreethiyadav579@gmail.com>
2022-09-18 21:30:26 +0530

    adding

--- robo/ec2/route53.tf
+++ robo/ec2/route53.tf
@@ -1,7 +1,6 @@
 resource "aws_route53_record" "component" {
-  name                = "${var.COMPONENT}-dev.robotshop.internal"
   zone_id             = "Z037286228DFYMBZCZ58K"
-  allow_overwrite     = true
+  name                = "${var.COMPONENT}-dev.robotshop.internal"
   type                = "A"
   ttl                 = 60
   records             = [aws_spot_instance_request.cheap-worker.private_ip]
