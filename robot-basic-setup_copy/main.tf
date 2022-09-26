0f7e0e3839131d2b4dbe486a806be89809c0ee50
preethi k <kpreethiyadav579@gmail.com>
2022-09-26 21:39:48 +0530

    adding

--- robot-basic-setup_copy/main.tf
+++ robot-basic-setup_copy/main.tf
@@ -2,5 +2,5 @@ module "frontend" {
     for_each     = var.ALL_COMPONENTS
     source       = "./ec2"
     COMPONENT    = each.key
-    app_version  = each.value.app_version
+    APP_VERSION  = each.value.app_version
 }
