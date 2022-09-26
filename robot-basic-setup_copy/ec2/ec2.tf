689ee790ea91205db1cecfd205276c3583f7895e
preethi k <kpreethiyadav579@gmail.com>
2022-09-26 21:32:32 +0530

    adding

--- robot-basic-setup_copy/ec2/ec2.tf
+++ robot-basic-setup_copy/ec2/ec2.tf
@@ -1,8 +1,8 @@
-resource "aws_spot_instance_request" "frontend" {
-  ami                      = data.aws_ami.ami-with-ansible.image_id
-  instance_type            = "t3.micro"
-  wait_for_fulfillment     = true
-  vpc_security_group_ids   = [aws.aws_security_group.allow_all_preek.id]
+resource "aws_spot_instance_request" "cheap_worker" {
+  ami                    = data.aws_ami.AMI-Ansible-with-base.image_id
+  instance_type          = "t3.micro"
+  wait_for_fulfillment   = true 
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
 
   tags = {
     Name = var.COMPONENT
@@ -13,17 +13,16 @@ resource "null_resource" "null" {
   triggers = {    
         a = timestamp()  # Everytime you run, when compared to the last time, the time changes, so it will be triggered all the time.
   }
- provisioner "remote-exec" {
-
+  provisioner "remote-exec" {
       connection {
         type     = "ssh"
         user     = "centos"
         password = "DevOps321"
-        host     = self.public_ip
-      }
-
+        # host     = self.public_ip
+        host     = aws_spot_instance_request.cheap_worker.private_ip 
+      } 
     inline = [
-        "ansible-pull -U https://github.com/kalindalapreethiyadav/Ansible.git roboshop.yml -e COMPONENT=${var.COMPONENT} -e ENV=dev -e TAG_NAME=${var.APP_VERSION}"
+     "ansible-pull -U https://github.com/kalindalapreethiyadav/Ansible.git roboshop.yml -e COMPONENT=${var.COMPONENT} -e ENV=dev -e TAG_NAME=${var.APP_VERSION}"
       ]
     }
 }
